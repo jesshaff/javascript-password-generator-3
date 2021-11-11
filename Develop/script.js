354c5a69ae5ca3937fd25e76dc0df9ab9c4bfbbc
jessicahaff@yahoo.com <jessicahaff@yahoo.com>
2021-11-10 19:28:26 -0500

    adding variables for password to generate

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var password = document.getElementById("password");
+// Set variables
+var abcUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var abcLowercase = "abcdefghijklmnopqrstuvwxyz";
+var numeric = "0123456789";
+var specialChars = "!@#$%^&*()_+,-=+`~[]{}/";
+
 
 // Write password to the #password input
 function writePassword() {

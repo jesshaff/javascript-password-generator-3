70d4723b07984c6e2fccd6f7d61f3a5afa0610c3
jessicahaff@yahoo.com <jessicahaff@yahoo.com>
2021-11-10 19:41:20 -0500

    welcome prompt and console.log for button being clicked added

--- Develop/script.js
+++ Develop/script.js
@@ -7,7 +7,13 @@ var abcLowercase = "abcdefghijklmnopqrstuvwxyz";
 var numeric = "0123456789";
 var specialChars = "!@#$%^&*()_+,-=+`~[]{}/";
 
+// Prompt for user to begin selecting criteria to generate password
+window.onload = alert("Welcome! Select 'Generate Password' to generate your random password! :)")
 
+// Verification that Generate Password button works 
+function generatePassword() {
+  console.log("The 'Generate Password' button has been clicked.");
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();

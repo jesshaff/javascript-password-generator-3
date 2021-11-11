96983579376da92e3ddc74f2e8bc6db06da0ffd9
jessicahaff@yahoo.com <jessicahaff@yahoo.com>
2021-11-10 21:06:05 -0500

    adjustments made to help get code to run off prompt answers - wip

--- Develop/script.js
+++ Develop/script.js
@@ -7,29 +7,30 @@ var abcLowercase = "abcdefghijklmnopqrstuvwxyz";
 var numeric = "0123456789";
 var specialChars = "!@#$%^&*()_+,-=+`~[]{}/";
 
+
 // Prompt for user to begin selecting criteria to generate password
 window.onload = alert("Welcome! Select 'Generate Password' to generate your random password! :)")
 
 // Verification that Generate Password button works 
 function generatePassword() {
   console.log("The 'Generate Password' button has been clicked.");
-  /* Prompt the user for password criteria 
-    length between 8-128, lowercase, uppercase, numeric, special characters */
+  // Prompt the user for password length criteria 
   var length = parseInt(prompt("How many characters would you like your password to contain? Must be between 8 and 128 characters."));
-
-  if (length < 8) {
-    alert('The password must be at least 8 characters.');
-    return "Password will generate and display here once more than 8 characters is selected.";
+    if (length < 8) {
+      alert('The password must be at least 8 characters.');
+      return "Password will generate and display here once more than 8 characters is selected.";
     }
-  if (length > 128) {
-    alert('The password must be less than 128 characters.');
-    return "Password will generate and display here once less than 128 characters is selected.";
+    if (length > 128) {
+      alert('The password must be less than 128 characters.');
+      return "Password will generate and display here once less than 128 characters is selected.";
     }
-  /* Validate the input */
+  // Prompt the user for characters
+  var includeUppercase = confirm("Do you want your password to include uppercase characters?");
+  var includeLowercase = confirm("Do you want your password to include lowercase characters?");
+  var includeNumeric = confirm("Do you want your password to include numeric characters?");
+  var includeSpecialChars = confirm("Do you want your password to include special characters?");
 
-  /* Display password to the page */
-
-  return "Password will generate and display here once required criteria is selected"
+  return "Password will generate and display here once all required criteria is selected"
 }
 
 // Write password to the #password input
